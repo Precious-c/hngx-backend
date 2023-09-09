6afa1685c5e1b1f847f5695845bf3319bf695218
Precious-c <preciousamaechi111@yahoo.com>
2023-09-09 15:29:56 +0100

    Updated github links

--- stage-one/server.js
+++ stage-one/server.js
@@ -6,14 +6,17 @@ const date = new Date()
 const options = {weekday: 'long'};
 let currentDay = date.toLocaleDateString('en-US', options);
 
+app.get('/', (req, res) => {
+    res.send("Usage Endpoint: https://hngx-backend1.onrender.com/api/api?slack_name=example_name&track=backend")
+}) 
 app.get('/api', (req, res) => {
     res.json({
         slack_name: req.query.slack_name,
         currentDay: currentDay,
         utc_time: date.toISOString(),
         track: req.query.track,
-        github_file_url: '',
-        github_repo_url: '',
+        github_file_url: 'https://github.com/Precious-c/hngx-backend/blob/main/stage-one/server.js',
+        github_repo_url: 'https://github.com/Precious-c/hngx-backend/tree/main/stage-one',
         status_code: 200
     })
 }) 
